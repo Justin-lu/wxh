280e265cc301e613ef573814e3ebf5cbc63d0002
luxiaoyong <gdjyluxiaoyong@gmail.com>
2013-04-01 11:14:52 +0800

    update

--- js/fool.js
+++ js/fool.js
@@ -1,7 +1,7 @@
 (function fool(){ 
     for(var i=0; i<40; i++ ){ 
       var count = 40 - i;
-      window.open(alert("好吧，你再数" + count + "下，就可以关掉啦" );)
+      window.open(alert("好吧，你再数" + count + "下，就可以关掉啦" ))
     }
     alert("o(∩∩)o...哈哈,还没有结束呢!!");
     alert("愚人节快乐!!");
