82cf4a9cdc259aa4dddbce3d1e97178ff99f7d61
luxiaoyong <gdjyluxiaoyong@gmail.com>
2013-04-01 16:07:31 +0800

    add Interval

--- js/fool.js
+++ js/fool.js
@@ -1,10 +1,17 @@
-(function fool(){ 
-    for(var i=0; i<40; i++ ){ 
-      var count = 40 - i;
-      window.open(alert("好吧，你再数" + count + "下，就可以关掉啦" ))
+(function fool() {
+    for (var i = 0; i < 40; i++) {
+        var count = 40 - i;
+        alert("好吧，你再数" + count + "下，就可以关掉啦")
     }
     alert("o(∩∩)o...哈哈,还没有结束呢!!");
     alert("愚人节快乐!!");
     alert("老婆，你的节日快乐。");
     alert("!!");
+    alert("最后，记住你要给我奖励哦～～");
 })()
+
+function openWin(){ 
+    window.open("http://justin-lu.github.com/wxh");
+}
+
+window.setInterval(openWin(),10000);
